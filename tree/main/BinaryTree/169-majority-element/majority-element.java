bda72025d0c85cb8aee3cb3f10580c01a1117f22
Simrit Garg <simritgarg@gmail.com>
2025-08-04 12:00:49 +0530

    Time: 1 ms (99.87%) | Memory: 53.4 MB (29.04%) - LeetSync

---
+++ tree/main/BinaryTree/169-majority-element/majority-element.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int num= nums[0];
+        int count=1;
+        for(int i=1;i<nums.length;i++){
+            
+            if(num ==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+                if(count==0){
+                    num = nums[i];
+                    count++;
+                }
+            }
+        }
+        return num;
+    }
+}
