7ccfab7703f5b94fbb0ea8ceb8a16395c3cd2726
Simrit Garg <simritgarg@gmail.com>
2025-09-04 01:15:47 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (8.54%) - LeetSync

--- tree/main/BinaryTree/42-trapping-rain-water/trapping-rain-water.java
+++ tree/main/BinaryTree/42-trapping-rain-water/trapping-rain-water.java
@@ -2,47 +2,47 @@ class Solution {
     public int trap(int[] height) {
         // with 2P
         int n = height.length;
-        // int left = 0;
-        // int right = n-1;
-        // int leftVal = 0;
-        // int rightVal = 0;
-        // int max = 0;
-        // while(left<right){
-        //     if(height[left]<height[right]){
-        //         if(height[left]>=leftVal){
-        //             leftVal = height[left];
-        //         }
-        //         else{
-        //             max += leftVal - height[left];
-        //         }
-        //         left++;
-        //     }
-        //     else{
-        //         if(height[right]>=rightVal){
-        //             rightVal = height[right];
-        //         }
-        //         else{
-        //             max += rightVal - height[right];
-        //         }
-        //         right--;
-        //     }
-        // }
-        // return max;
-
-        // using montonic stack
-        Stack<Integer> s = new Stack<>();
+        int left = 0;
+        int right = n-1;
+        int leftVal = 0;
+        int rightVal = 0;
         int max = 0;
-        for(int i=0;i<n;i++){
-            while(!s.isEmpty() && height[i]>height[s.peek()]){
-                int top = s.pop();
-                if(s.isEmpty()) break;
-                int b = i - s.peek() - 1;
-                int h = Math.min(height[i], height[s.peek()]) - height[top];
-                max += b*h;
-                
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]>=leftVal){
+                    leftVal = height[left];
+                }
+                else{
+                    max += leftVal - height[left];
+                }
+                left++;
             }
-            s.push(i);
+            else{
+                if(height[right]>=rightVal){
+                    rightVal = height[right];
+                }
+                else{
+                    max += rightVal - height[right];
+                }
+                right--;
+            }
         }
         return max;
+
+        // using montonic stack
+        // Stack<Integer> s = new Stack<>();
+        // int max = 0;
+        // for(int i=0;i<n;i++){
+        //     while(!s.isEmpty() && height[i]>height[s.peek()]){
+        //         int top = s.pop();
+        //         if(s.isEmpty()) break;
+        //         int b = i - s.peek() - 1;
+        //         int h = Math.min(height[i], height[s.peek()]) - height[top];
+        //         max += b*h;
+                
+        //     }
+        //     s.push(i);
+        // }
+        // return max;
     }
 }
