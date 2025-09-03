e4d1e7350c12c83c39c587e29770b6e8e0e3e6af
Simrit Garg <simritgarg@gmail.com>
2025-09-04 01:15:31 +0530

    Time: 6 ms (5.46%) | Memory: 45.7 MB (97.31%) - LeetSync

--- tree/main/BinaryTree/42-trapping-rain-water/trapping-rain-water.java
+++ tree/main/BinaryTree/42-trapping-rain-water/trapping-rain-water.java
@@ -2,30 +2,46 @@ class Solution {
     public int trap(int[] height) {
         // with 2P
         int n = height.length;
-        int left = 0;
-        int right = n-1;
-        int leftVal = 0;
-        int rightVal = 0;
+        // int left = 0;
+        // int right = n-1;
+        // int leftVal = 0;
+        // int rightVal = 0;
+        // int max = 0;
+        // while(left<right){
+        //     if(height[left]<height[right]){
+        //         if(height[left]>=leftVal){
+        //             leftVal = height[left];
+        //         }
+        //         else{
+        //             max += leftVal - height[left];
+        //         }
+        //         left++;
+        //     }
+        //     else{
+        //         if(height[right]>=rightVal){
+        //             rightVal = height[right];
+        //         }
+        //         else{
+        //             max += rightVal - height[right];
+        //         }
+        //         right--;
+        //     }
+        // }
+        // return max;
+
+        // using montonic stack
+        Stack<Integer> s = new Stack<>();
         int max = 0;
-        while(left<right){
-            if(height[left]<height[right]){
-                if(height[left]>=leftVal){
-                    leftVal = height[left];
-                }
-                else{
-                    max += leftVal - height[left];
-                }
-                left++;
+        for(int i=0;i<n;i++){
+            while(!s.isEmpty() && height[i]>height[s.peek()]){
+                int top = s.pop();
+                if(s.isEmpty()) break;
+                int b = i - s.peek() - 1;
+                int h = Math.min(height[i], height[s.peek()]) - height[top];
+                max += b*h;
+                
             }
-            else{
-                if(height[right]>=rightVal){
-                    rightVal = height[right];
-                }
-                else{
-                    max += rightVal - height[right];
-                }
-                right--;
-            }
+            s.push(i);
         }
         return max;
     }
