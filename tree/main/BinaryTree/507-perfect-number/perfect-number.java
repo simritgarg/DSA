7feb9e37ffe94e4785cc7ef1ef5a19500797f407
Simrit Garg <simritgarg@gmail.com>
2025-09-10 23:22:55 +0530

    Time: 2 ms (77.69%) | Memory: 40.8 MB (21.11%) - LeetSync

--- tree/main/BinaryTree/507-perfect-number/perfect-number.java
+++ tree/main/BinaryTree/507-perfect-number/perfect-number.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        if(num <= 1) return false;
         int sum = 1;
-        for(int i=2;i<num;i++){
-            if(num%i==0) sum += i;
+        for(int i=2;i<Math.sqrt(num);i++){
+            if(num%i==0){
+                 sum += i;
+                if(i != num/i) sum += num/i;
+            }
         }
-        return sum == num && num != 1;
+        return sum == num;
     }
 }
