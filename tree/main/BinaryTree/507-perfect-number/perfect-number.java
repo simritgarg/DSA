f40b29290dd16357f18818cf8634a7539f093045
Simrit Garg <simritgarg@gmail.com>
2025-09-10 23:19:15 +0530

    Time: 2103 ms (18.23%) | Memory: 40.9 MB (6.88%) - LeetSync

--- tree/main/BinaryTree/507-perfect-number/perfect-number.java
+++ tree/main/BinaryTree/507-perfect-number/perfect-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if(num <= 1) return false;
-        int sum = 0;
-        for(int i=1;i<num;i++){
+        int sum = 1;
+        for(int i=2;i<num;i++){
             if(num%i==0) sum += i;
         }
-        return sum == num;
+        return sum == num && num != 1;
     }
 }
