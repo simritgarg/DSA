15f8072407d8dc03d1de8551ccfb057e12560211
Simrit Garg <simritgarg@gmail.com>
2025-09-11 00:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (86.06%) - LeetSync

--- tree/main/BinaryTree/202-happy-number/happy-number.java
+++ tree/main/BinaryTree/202-happy-number/happy-number.java
@@ -1,20 +1,31 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
-        while(n!=1){
-           if (seen.contains(n)) return false;
-            seen.add(n);
-            n = sqsum(n);
-        }
-        return true;        
+    //     Set<Integer> seen = new HashSet<>();
+    //     while(n!=1){
+    //        if (seen.contains(n)) return false;
+    //         seen.add(n);
+    //         n = sqsum(n);
+    //     }
+    //     return true;        
+    // }
+    // public int sqsum(int n){
+    //         int sum = 0;
+    //         while(n>0){
+    //             int digit = n%10;
+    //             sum += digit*digit;
+    //             n /= 10;
+    //         }
+    //         return sum;
+    //     }
+    if(n == 1) return true;
+    if(n == 4) return false;
+    int sum = 0;
+    while(n>0){
+        int digit = n%10;
+        sum += digit*digit;
+        n /= 10;
     }
-    public int sqsum(int n){
-            int sum = 0;
-            while(n>0){
-                int digit = n%10;
-                sum += digit*digit;
-                n /= 10;
-            }
-            return sum;
-        }
+    return isHappy(sum);
+    }
+
 }
